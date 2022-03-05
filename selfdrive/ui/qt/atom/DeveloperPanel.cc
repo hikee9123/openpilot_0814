901c5b49e3d92a783f86398ba3d1d895331befcc
hikee9123 <hikee9123@gmail.com>
2022-03-05 23:47:13 +0900

    fix

--- selfdrive/ui/qt/atom/DeveloperPanel.cc
+++ selfdrive/ui/qt/atom/DeveloperPanel.cc
@@ -204,7 +204,7 @@ DeveloperPanel::DeveloperPanel(QWidget* parent) : QFrame(parent)
   auto laserlevel_exe = new ButtonControl("laserlevel Open", "Open","laserlevel을 실행 합니다.");
   connect(laserlevel_exe, &ButtonControl::clicked, [=]() 
   { 
-    std::system("aam start com.goodappsoftware.laserlevel/com.goodappsoftware.laserlevel.MainActivity");
+    std::system("am start com.goodappsoftware.laserlevel/com.goodappsoftware.laserlevel.MainActivity");
   });  
 
 
