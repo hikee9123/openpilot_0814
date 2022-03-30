5afe803b23ff88add4835c1e600d87f6e4ed6675
hikee9123 <hikee9123@gmail.com>
2022-03-30 15:11:51 +0900

    gain

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -70,10 +70,11 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.init('lqr')
       ret.lateralTuning.lqr.scale = 2000     #1700.0
       ret.lateralTuning.lqr.ki = 0.01      #0.01
-      ret.lateralTuning.lqr.dcGain =  0.0027  #0.0027  #0.00285   # 0.002237852961363602
+      ret.lateralTuning.lqr.dcGain =  0.0027  #0.0027
       # 호야  1500, 0.015, 0.0027
       #  1700, 0.01, 0.0029
       #  2000, 0.01, 0.003
+      # toyota  1500, 0.05,   0.002237852961363602
   
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
